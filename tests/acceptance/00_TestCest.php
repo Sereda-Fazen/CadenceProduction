d4c0a865d497ce61f71e328b597cba274905dc66
Sereda-Fazen <fazen7@mail.ru>
2016-04-14 10:40:15 +0300

    130 prod - 3

--- tests/acceptance/00_TestCest.php
+++ tests/acceptance/00_TestCest.php
@@ -5,12 +5,11 @@ class TestCest
 
 
     function loginSuccess(AcceptanceTester $I, \Page\Login $loginPage) {
-        $loginPage->login('cadence.test01@yahoo.com', '123456');
-        $I->amOnPage('/customer/account/index/');
+        $loginPage->login('cadence_test@yahoo.com', '123456');
+        //$I->amOnPage('/customer/account/index/');
         $I->see('Hello, alex sereda!', 'p.hello > strong');
         $loginPage->logout();
     }
-
 
 
 
